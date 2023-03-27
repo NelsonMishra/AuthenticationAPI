e0fa0b949225cb75dd4d8c1650fc2d9efa78d132
joker <58170697+NelsonMishra@users.noreply.github.com>
2023-03-27 21:20:17 +0530

    Add files via upload
    first commit , authentication process..

---
+++ spring-security-test/src/main/java/com/example/springsecuritytest/HomeController/HomeController.java
@@ -0,0 +1,14 @@
+package com.example.springsecuritytest.HomeController;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	
+	@GetMapping("/")
+	public String home() {
+		return "WELCOME TO HOME PAGE";
+	}
+
+}
